651a05399bce2c8f5ff4d7e3c9aa8c8f6a26a984
OpenSourceDoc <cwchen.tw@outlook.com>
2026-05-25 06:34:03 +0800

    Fix a tool script

--- setting.sh
+++ setting.sh
@@ -1,6 +1,22 @@
+IS_WINDOWS ()
+{
+    case "$(uname -s)" in
+        CYGWIN*|MINGW*|MSYS*)
+            return 0
+            ;;
+        *)
+            return 1
+            ;;
+    esac
+}
+
 ENTRY=Main.lpr
-PROGRAM=cdsl
-EXECUTABLE="$PROGRAM"
+PROGRAM=cli
+if IS_WINDOWS; then
+    EXECUTABLE="${PROGRAM}.exe"
+else
+    EXECUTABLE="$PROGRAM"
+fi
 
 CWD=$(CDPATH='' cd -- "$(dirname -- "$0")" && pwd) || exit 1
 
